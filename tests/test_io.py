3edfc10d142df14031d6c63e006af3d27fdc1a96
Jouni Kiviperä <jounikivipera@gmail.com>
2026-05-08 10:48:16 +0300

    lisäyksiä

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,1 +1,10 @@
+from pathlib import Path
 
+import pytest
+
+from job_application_assistant.io import read_text_file
+
+
+def test_read_text_file() -> None:
+  path = Path(__file__).parent / "test_file.txt"
+  assert read_text_file(path) == "Hello world"
